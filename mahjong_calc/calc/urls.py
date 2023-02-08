e45141cf8c4213acdb9aafc9d1e095bbff3ae40d
{mknnishiko} <{mkn.nishiko@gmail.com}>
2023-02-08 22:32:47 +0900

    [add] index page

--- mahjong_calc/calc/urls.py
+++ mahjong_calc/calc/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import index
 
 urlpatterns = [
-    path('index/', views.index, name = 'index')
+    path('index/', index.View.as_view(), name = 'index')
 ]
